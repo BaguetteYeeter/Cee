6ee1828b77cd421932ea191292405c36ceddfcd6
BaguetteYeeter <baguetteyeeter@gmail.com>
2020-12-04 21:11:06 +0000

    Added Integers

--- main.c
+++ main.c
@@ -1,5 +1,11 @@
 #include <stdio.h>
 
 void main (void) {
-  printf("Hello World!\n");
+  int a;
+  int b = 3;
+  int c;
+
+  a = 2;
+  c = a + b;
+  printf("The sum of %d and %d is %d\n", a, b, c);
 }
